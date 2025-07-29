fb48e91a2c589ee765e6a634760e61e9b0c56ef0
killakilla1609-png <killakilla1609@gmail.com>
2025-07-29 19:10:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>web kelompok-11</title>
+</head>
+
+<body>
+<center>
+
+<h1>SELAMAT DATANG DI WEBSITE KAMI KEL-11</h1>
+<h3>Nama: Shakilla Zahra</h3>
+<h3>NPM:1224057</h3>
+<p></p>
+<h3>Nama: Tiara Ramadhani</h3>
+<h3>NPM:1224042</h3>
+<p></p>
+<h3>Nama: Andri Apriandi</h3>
+<h3>NPM:1224029</h3>
+<p></p>
+<h5> Created @2025 By kel-11</h5>
+</creat>
+</body>
+</html>
